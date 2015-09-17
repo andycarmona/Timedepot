6d7cfec878c03fdd67407be8937a658f0b4a9bf3
andycarmona <andres.carmona@cybercom.com>
2015-09-17 11:35:33 +0200

    Ship from data in #enter sipment" shouldn't be editable

--- Global.asax.cs
+++ Global.asax.cs
@@ -27,7 +27,7 @@ namespace TimelyDepotMVC
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
